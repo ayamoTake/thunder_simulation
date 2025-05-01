c704639ae2f2ef2f90441a223241744153d38fc5
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-05-02 08:00:28 +0900

    Second completed

--- Board.hpp
+++ Board.hpp
@@ -3,116 +3,192 @@
 #include<vector>
 #include<algorithm>
 #include<queue>
+#include<cmath>
+#include<random>
+#include<set>
+
+/*
+ * Issue: 袋小路になると終わる()
+ * */
 
 using namespace std;
 struct Index {
     int r, c;
 };
 
+inline bool operator<(const Index& a, const Index&b) {
+    if (a.r != b.r) return a.r < b.r;
+    return a.c < b.c;
+}
+
 class Cell {
 public:
     bool is_thunder = false;
-    int e = 0;
-    int prob = 0;
+    int around_ct = 0;
 };
 
 class Board {
 public:
     vector<vector<Cell>> cells;
-    Board(int n) : comp(this), _Q(comp), pushed(n, vector<bool>(n, false)) { _init(n); }
-    void add_as_thunder(const Index& idx);
+    Board(int n, double eta) : eta(eta), pushed(n, vector<bool>(n, false)) { _init(n); }
     Index select();
-    int prob (const Index& idx) const;
+    bool add_as_thunder(const Index& idx);
+    double prob (const Index& idx) const;
+    int e(const Index& idx) const;
+    int e2(const Index& idx) const;
     void print() const;
-    bool ended() const { return _Q.empty(); }
+    void d_print() const;
+    bool ended() const { return will.empty(); }
+    int row_size;
+    int col_size;
+    double eta = 3;
 private:
-    struct Comp {
-        Board* parent;
-        Comp (Board* p) : parent(p) {}
-        bool operator()(const Index& a, const Index& b) const {
-            return parent->prob(a) > parent->prob(b);
-        }
-    };
-    Comp comp;
-    priority_queue<Index, vector<Index>, Comp> _Q;
     vector<Index> four_neighbor(const Index& tar) const;
     vector<vector<bool>> pushed;
+    set<Index> _init_cell;
+    deque<Index> will;
+    mutable mt19937 rand_gen;
+
     bool ok(const Index& tar) const;
     void _init(int n);
 };
 
 inline bool Board::ok(const Index& idx) const {
-    return idx.r >= 0 && idx.r < cells.size()
-        && idx.c >= 0 && idx.c < cells.size();
+    return idx.r >= 0 && idx.r < row_size
+        && idx.c >= 0 && idx.c < col_size;
 };
 
 inline vector<Index> Board::four_neighbor(const Index& idx) const {
     vector<Index> ans;
-    vector<vector<int>> diff = { {0, -1}, {-1, 0}, {0, 1}, {1, 0}, };
+    vector<vector<int>> diff = { {0, 1}, {0, -1}, {1, 0}, {-1, 0}, };
     for (const auto& d : diff) {
         Index tmp = {idx.r + d[0], idx.c + d[1]};
+        if (tmp.c == -1) tmp.c = col_size - 1;
+        else if (tmp.c == col_size) tmp.c = 0;
         if (ok(tmp)) {
             ans.push_back(tmp);
         }
     }
+    shuffle(ans.begin(), ans.end(), rand_gen);
     return ans;
 }
 
-inline void Board::add_as_thunder(const Index& idx) {
-    if (cells[idx.r][idx.c].is_thunder 
-        // && !(idx.r == 1 && idx.c == cells.size() / 2)
-    ) {
+inline bool Board::add_as_thunder(const Index& idx) {
+    if (cells[idx.r][idx.c].is_thunder) {
         cerr << "have already been thunder\n";
         cerr << idx.r << ", " << idx.c << endl;
         exit(1);
     }
-    cells[idx.r][idx.c].e = 4;
     cells[idx.r][idx.c].is_thunder = true;
+    cells[idx.r][idx.c].around_ct = 4;
     for (const Index& nei: four_neighbor(idx)) {
         if (
-            !cells[nei.r][nei.c].is_thunder 
-            && nei.r != 0 && !(nei.r == 1 && nei.c == cells.size() / 2)
-            && !pushed[nei.r][nei.c]
-        ) {
-            cells[nei.r][nei.c].e++;
-            _Q.push(nei);
+            cells[nei.r][nei.c].is_thunder 
+            || _init_cell.find(nei) != _init_cell.end()
+        ) continue;
+
+        cells[nei.r][nei.c].around_ct++;
+        if (!pushed[nei.r][nei.c]) {
+            if (will.size() >= 10) {
+                will.pop_front();
+            }
+            will.push_back(nei);
             pushed[nei.r][nei.c] = true;
         }
     }
+
+    return idx.r == row_size - 1;
 }
 
-inline int Board::prob(const Index& idx) const {
+inline int Board::e(const Index& idx) const {
+    return cells[idx.r][idx.c].around_ct;
+}
+
+inline int Board::e2(const Index& idx) const {
+    if (!ok(idx)) return 0;
+    if (cells[idx.r][idx.c].is_thunder) return 1;
     int sum = 0;
-    for (auto nei: four_neighbor(idx)) {
-        sum += cells[nei.r][nei.c].e;
+    for (const Index& nei: four_neighbor(idx)) {
+        sum += this->e(nei);
     }
     return sum;
 }
 
+inline double Board::prob(const Index& idx) const {
+    return pow(1 - e2(idx) / 16.0, eta) * 100;
+}
+
 inline void Board::_init(int n) {
-    cells.assign(n, vector<Cell>(n));
-    for (int i = 0; i < n; i++) {
-        add_as_thunder({0, i});
+    row_size = n;
+    col_size = n / 9 * 16;
+    cells.assign(row_size, vector<Cell>(col_size));
+
+    for (int i = 0; i < col_size; i++) {
+        _init_cell.insert({0, i});
     }
-    int sx = n / 2;
-    add_as_thunder({1, sx});
+    int sx = col_size / 2;
+    _init_cell.insert({1, sx});
+
+    for (const Index& idx: _init_cell) {
+        add_as_thunder(idx);
+    }
 }
 
 inline Index Board::select() {
-    Index ans = _Q.top();
-    _Q.pop();
+    vector<decltype(will.begin())> target;
+    for (auto it = will.begin(); it != will.end(); it++) {
+        target.push_back(it);
+    }
+    int n = target.size();
+    vector<double> ps(n);
+    for (int i = 0; i < n; i++) {
+        ps[i] = prob(*target[i]);
+    }
+    vector<double> sums(will.size());
+    partial_sum(ps.begin(), ps.end(), sums.begin());
+
+    uniform_real_distribution<> dist(0.0, sums[n - 1]);
+    int r = dist(rand_gen);
+    int ans_idx = lower_bound(sums.begin(), sums.end(), r) - sums.begin();
+
+    Index ans = *target[ans_idx];
+    /* 
+     * erase は線形時間. timestamp を使ったmin-heap でできる??
+     * */
+    will.erase(target[ans_idx]); 
+
     return ans;
+}
+
+inline void Board::d_print() const {
+    cout << "\033[2J";
+
+    double sum = 0;
+    set<Index> will_set;
+    for ( auto idx : will) {
+        will_set.insert(idx);
+        double p = prob(idx);
+        sum += p;
+        // cout << p << endl;
+    }
+    // cout << "sum: " << sum << endl;
+
+    for (int i = 0; i < row_size; i++) {
+        for (int j = 0; j < col_size; j++) {
+            if (cells[i][j].is_thunder) {
+                printf("\033[33m███\033[0m");
+            } else if (pushed[i][j]) {
+                printf("%3d", (int) (prob({i, j}) / sum * 100));
+            } else {
+                printf("   ");
+            }
+        }
+        cout << endl;
+    }
 }
 
 inline void Board::print() const {
     cout << "\033[2J";
-
-    auto q = _Q;
-    for (int i = 0; i < q.size(); i++) {
-        Index idx = q.top(); q.pop();
-        cout << "(" << idx.r << ", " << idx.c << ") ";
-    }
-    cout << endl;
 
     for (const auto& r: cells) {
         for (const auto& c: r) {
--- main.cpp
+++ main.cpp
@@ -1,17 +1,60 @@
 #include "Board.hpp"
+#include <cstring>
+#include <chrono>
+#include <thread>
+
+void do_live(Board& board, bool debug_mood) {
+    bool is_reached = false;
+    while(!is_reached) {
+        Index next = board.select();
+        is_reached = board.add_as_thunder(next);
+        if (debug_mood)
+            board.d_print();
+        else 
+            board.print();
+        this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+}
 
 int main(int argc, char** argv) {
     int n = 11; 
-    if (argc >= 2) {
-        n = std::stoi(argv[1]);
+
+    bool debug_mood = false;
+    bool live_mood = false;
+    double eta = 5;
+
+    for (int i = 1; i < argc; i++) {
+        if (std::strcmp(argv[i], "-d") == 0) {
+            debug_mood = true;
+        } else if (std::strcmp(argv[i], "-l") == 0) {
+            live_mood = true;
+        } else if (std::strcmp(argv[i], "-e") == 0) {
+            eta = stof(argv[++i]);
+        } else {
+            n = std::stoi(argv[i]);
+        }
     }
 
-    Board board(n);
+    Board board(n, eta);
 
-    for (char c; c != EOF && !board.ended(); c = getchar()) {
-    // for (;;) {
-        Index next = board.select();
-        board.add_as_thunder(next);
+    if (debug_mood)
+        board.d_print();
+    else 
         board.print();
+
+    if (live_mood) {
+        do_live(board, debug_mood);
+        return 0;
     }
+
+    bool is_reached = false;
+    for (char c; c != EOF && !board.ended() && !is_reached; c = getchar()) {
+        Index next = board.select();
+        is_reached = board.add_as_thunder(next);
+        if (debug_mood)
+            board.d_print();
+        else 
+            board.print();
+    }
+
 }
