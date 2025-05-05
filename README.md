6c0a1ab04e43e445f1b2dbbfcc31edb0f09d8528
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-05-05 21:13:58 +0900

    add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# 雷のシミュレーション
+
+これだけを観て, 再現しました
+
+[![雷の形をシミュレーションする方法](https://img.youtube.com/vi/3mVXibo4MN0/0.jpg)](https://www.youtube.com/watch?v=3mVXibo4MN0)
--- main.cpp
+++ main.cpp
@@ -42,8 +42,8 @@ int main(int argc, char** argv) {
     Board board(n, eta, seed);
 
     if (debug_mood)
-        // board.d_print();
-        board.print();
+        board.d_print();
+        // board.print();
     else 
         board.print();
 
